516c2b46d7109870f2c9f0dbe0ee8a021210d1cc
Zemke <florian.zemke@ewetel.net>
2013-09-15 12:45:09 +0200

    New shell to quickly add some applications for the testing phase.

---
+++ app/Console/Command/ApplyShell.php
@@ -0,0 +1,28 @@
+<?php
+
+class PopulateShell extends AppShell {
+
+    public $uses = array('User', 'Application');
+
+    public function main() {
+        $this->populateApplications();
+    }
+
+    private function populateApplications() {
+        // Starting off with 2, because I am already in.
+        for ($i = 2; $i <= 32; $i++) {
+            $this->Application->User->save(
+                array(
+                    'id' => $i,
+                    'stage' => 'applied'
+                )
+            );
+
+            $this->Application->save(
+                array(
+                    'user_id' => $i,
+                )
+            );
+        }
+    }
+}
