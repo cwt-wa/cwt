fd51aabdc8668dcb4cb6fb1734a994b0b4d79ddf
Zemke <Florian.Zemke@ewetel.net>
2015-09-04 13:09:36 +0200

    Added JS and CSS files to livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var livereload = require('gulp-livereload');
 
-const FILES_TO_WATCH = ['app/Controller/**/*.php', 'app/Model/**/*.php', 'app/View/**/*.ctp'];
+const FILES_TO_WATCH = ['app/Controller/**/*.php', 'app/Model/**/*.php', 'app/View/**/*.ctp',
+    'app/webroot/css/**/*.css', 'app/webroot/js/**/*.js'];
 
 gulp.task('php', function() {
     gulp.src(FILES_TO_WATCH)
