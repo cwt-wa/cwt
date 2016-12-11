50d4e63784e9e0f7994fd78b05c53a7b2e510aa3
Zemke <flzemke@gmail.com>
2016-12-11 20:51:10 +0100

    GH-93 Hello, World!

--- src/main/java/com/cwtsite/cwt/CwtApplication.java
+++ src/main/java/com/cwtsite/cwt/CwtApplication.java
@@ -1,12 +1,26 @@
 package com.cwtsite.cwt;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
+@Controller
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class CwtApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CwtApplication.class, args);
-	}
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello, World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CwtApplication.class, args);
+
+    }
 }
