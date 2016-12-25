9d8c20b07d5031e8cdb55629241d68f2fc755bd6
Zemke <flzemke@gmail.com>
2016-12-25 21:03:08 +0100

    GH-93 Setup webpack
    As according to https://angular.io/docs/ts/latest/guide/webpack.html

--- cwt-angular/webpack.config.js
+++ cwt-angular/webpack.config.js
@@ -1,16 +1,1 @@
-module.exports = {
-    entry: "src/main/java/webapp/app/app.ts",
-    output: {
-        filename: "target/bundle.js"
-    },
-    resolve: {
-        // Add '.ts' and '.tsx' as a resolvable extension.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
-    },
-    module: {
-        loaders: [
-            // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
-            {test: /\.tsx?$/, loader: "ts-loader"}
-        ]
-    }
-}
+module.exports = require('./config/webpack.dev.js');
