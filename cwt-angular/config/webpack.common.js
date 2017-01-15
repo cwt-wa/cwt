b6f9d8dc56db9744a8db031e6c3ab75ab5d5d075
Zemke <flzemke@gmail.com>
2017-01-15 18:13:18 +0100

    GH-94 Add Font Awesome

--- cwt-angular/config/webpack.common.js
+++ cwt-angular/config/webpack.common.js
@@ -26,7 +26,7 @@ module.exports = {
                 loader: 'html'
             },
             {
-                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
                 loader: 'file?name=assets/[name].[hash].[ext]'
             },
             {
