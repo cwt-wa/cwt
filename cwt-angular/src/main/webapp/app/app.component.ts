904fd99c9e10b9c13718f4828af417500ecf49d3
Zemke <flzemke@gmail.com>
2017-02-26 17:11:18 +0100

    GH-94 Close user panel on orientation change

--- cwt-angular/src/main/webapp/app/app.component.ts
+++ cwt-angular/src/main/webapp/app/app.component.ts
@@ -1,18 +1,29 @@
-import {Component, Inject} from "@angular/core";
+import {Component, Inject, ViewChild, AfterViewInit} from "@angular/core";
 import {StandaloneWebAppService} from "./_services/standalone-web-app.service";
+import {BrowserWindowRef} from "./_utils/browser-window-ref";
+import {NgbPopover} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
     selector: 'my-app',
     template: require('./app.component.html')
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+    @ViewChild("userPanelIcon")
+    private userPanelIcon: NgbPopover;
 
     public isNavCollapsed: boolean = true;
     public isAppleStandalone: boolean;
     public isStandalone: boolean;
 
-    constructor(@Inject(StandaloneWebAppService) private standaloneWebAppService: StandaloneWebAppService) {
+    constructor(@Inject(StandaloneWebAppService) private standaloneWebAppService: StandaloneWebAppService,
+                @Inject(BrowserWindowRef) private browserWindowRef: BrowserWindowRef) {
         this.isAppleStandalone = this.standaloneWebAppService.isAppleStandalone;
         this.isStandalone = this.standaloneWebAppService.isStandalone;
+    }
+
+    ngAfterViewInit(): void {
+        this.browserWindowRef.window.addEventListener("orientationchange", () => {
+            this.userPanelIcon.close();
+        });
     }
 }
