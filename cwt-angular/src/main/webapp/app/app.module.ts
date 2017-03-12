a06539a094152e4c0fbd7dbe27d18a03a934048d
Zemke <flzemke@gmail.com>
2017-03-12 23:16:47 +0100

    GH-93 Started off with routing

--- cwt-angular/src/main/webapp/app/app.module.ts
+++ cwt-angular/src/main/webapp/app/app.module.ts
@@ -4,14 +4,36 @@ import {AppComponent} from "./app.component";
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {WebAppViewService} from "./_services/web-app-view.service";
 import {GmtClockComponent} from "./gmt-clock.component";
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./home.component";
+import {PageNotFoundComponent} from "./page-not-found.component";
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'dulli',
+        component: HomeComponent
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent
+    }
+];
+
 
 @NgModule({
     imports: [
         BrowserModule,
-        NgbModule.forRoot()
+        NgbModule.forRoot(),
+        RouterModule.forRoot(appRoutes)
     ],
     declarations: [
         AppComponent,
+        HomeComponent,
+        PageNotFoundComponent,
         GmtClockComponent
     ],
     providers: [
