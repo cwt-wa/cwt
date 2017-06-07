d731203548d9d4f72ba3bc36fbe015df89d4c863
Zemke <flzemke@gmail.com>
2017-06-07 18:20:37 +0200

    Collapse on navigation

--- cwt-angular/src/main/webapp/app/app.component.ts
+++ cwt-angular/src/main/webapp/app/app.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component, ViewChild} from "@angular/core";
 import {WebAppViewService} from "./_services/web-app-view.service";
 import {NgbPopover} from "@ng-bootstrap/ng-bootstrap";
 import {GmtClockComponent} from "./gmt-clock.component";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'my-app',
@@ -16,12 +17,14 @@ export class AppComponent implements AfterViewInit {
     public isAppleStandalone: boolean;
     public isStandalone: boolean;
 
-    constructor(private webAppViewService: WebAppViewService) {
+    constructor(private webAppViewService: WebAppViewService, private router: Router) {
         this.isAppleStandalone = this.webAppViewService.isAppleStandalone;
         this.isStandalone = this.webAppViewService.isStandalone;
     }
 
     ngAfterViewInit(): void {
         this.webAppViewService.closeUserPanelOnOrientationChange(this.userPanelIcon);
+
+        this.router.setUpLocationChangeListener();
     }
 }
