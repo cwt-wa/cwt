b0f8096a0191ee901429e2fa1a0b6137d1124792
Zemke <flzemke@gmail.com>
2017-06-26 17:49:09 +0200

    Well, there's Toastr
    I guess it's not right to use require, but I have no other idea right
    now.
    
    Maybe here's an answer:
    https://stackoverflow.com/questions/28969861/managing-jquery-plugin-dependency-in-webpack

--- cwt-angular/src/main/webapp/app/register.component.ts
+++ cwt-angular/src/main/webapp/app/register.component.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {RequestService} from "./_services/request.service";
 import {UserRegistration} from "./user-registration";
+const toastr = require('toastr/toastr.js');
 
 @Component({
     selector: 'cwt-register',
@@ -15,7 +16,6 @@ export class RegisterComponent {
     }
 
     onSubmit() {
-        // http://codeseven.github.io/toastr/demo.html
         this.requestService.post('auth/register', this.userRegistration)
             .subscribe();
     }
--- cwt-angular/src/main/webapp/vendor.ts
+++ cwt-angular/src/main/webapp/vendor.ts
@@ -9,6 +9,8 @@ import "@angular/router";
 import 'bootstrap/dist/css/bootstrap.css';
 import '@ng-bootstrap/ng-bootstrap';
 import 'font-awesome/css/font-awesome.css';
+import 'toastr/toastr.js';
+import 'toastr/build/toastr.css';
 
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
