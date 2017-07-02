0f140506b684c9fdb1a7103f3208b45c29e34a7a
Zemke <flzemke@gmail.com>
2017-07-02 13:36:18 +0200

    Fix CORS filter setting

--- cwt-spring/src/main/java/com/cwtsite/cwt/security/filter/CorsFilter.java
+++ cwt-spring/src/main/java/com/cwtsite/cwt/security/filter/CorsFilter.java
@@ -18,7 +18,7 @@ public class CorsFilter extends OncePerRequestFilter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "content-type");
         chain.doFilter(request, response);
     }
 }
