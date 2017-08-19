d978554ba55be33b871e8729169ac2999e1474d2
Zemke <flzemke@gmail.com>
2017-08-19 12:14:32 +0200

    Prepare configuration persistence

---
+++ cwt-spring/src/main/java/com/cwtsite/cwt/entity/Configuration.java
@@ -0,0 +1,71 @@
+package com.cwtsite.cwt.entity;
+
+
+import com.cwtsite.cwt.user.repository.entity.User;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "configuration")
+public class Configuration implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "key")
+    private String key;
+
+    @Column(name = "value")
+    private String value;
+
+    @Column(name = "created")
+    private LocalDate created;
+
+    @ManyToOne
+    private User author;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDate created) {
+        this.created = created;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+}
---
+++ cwt-spring/src/main/java/com/cwtsite/cwt/entity/enumeration/ConfigurationKey.java
@@ -0,0 +1,6 @@
+package com.cwtsite.cwt.entity.enumeration;
+
+public enum ConfigurationKey {
+
+    // TODO All possible configurations should have their key specified here.
+}
