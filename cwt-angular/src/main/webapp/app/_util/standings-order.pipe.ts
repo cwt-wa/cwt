f719a54e11dcb1e22b23b93aebc82d08079213cc
Zemke <flzemke@gmail.com>
2017-08-26 23:18:06 +0200

    Order standings

---
+++ cwt-angular/src/main/webapp/app/_util/standings-order.pipe.ts
@@ -0,0 +1,20 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {GroupStanding} from "../custom";
+
+/**
+ * Order standings by points > game ratio > round ratio > head-to-head record.
+ *
+ * @todo Use <a href="https://angular.io/api/common/AsyncPipe">AsyncPipe</a> to do head-to-head record.
+ */
+@Pipe({
+    name: 'cwtStandingsOrder'
+})
+export class StandingsOrderPipe implements PipeTransform {
+
+    transform(standings: GroupStanding[]): GroupStanding[] {
+        return standings
+            .sort((a, b) => a.roundRatio > b.roundRatio ? -1 : +1)
+            .sort((a, b) => a.gameRatio > b.gameRatio ? -1 : +1)
+            .sort((a, b) => a.points > b.points ? -1 : +1);
+    }
+}
