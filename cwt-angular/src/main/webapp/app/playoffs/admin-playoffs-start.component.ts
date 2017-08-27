de3af2293febf098bafe20934b352d9a2e161d34
Zemke <flzemke@gmail.com>
2017-08-27 12:56:10 +0200

    Typeahead for users

--- cwt-angular/src/main/webapp/app/playoffs/admin-playoffs-start.component.ts
+++ cwt-angular/src/main/webapp/app/playoffs/admin-playoffs-start.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {RequestService} from "../_services/request.service";
-import {Configuration, GameDto, Group, User} from "../custom";
+import {Configuration, Game, Group, User} from "../custom";
 import {ConfigurationService} from "../_services/configuration.service";
 import {Observable} from "rxjs/Observable";
+import {StandingsOrderPipe} from "../_util/standings-order.pipe";
 
 @Component({
     selector: 'cwt-admin-playoffs-start',
@@ -12,25 +13,32 @@ export class AdminPlayoffsStartComponent implements OnInit {
 
     public groups: Group[];
     public numberOfGroupMembersAdvancing: number;
-    public games: GameDto[];
-    private typeAheadForGroupMember: (text$: Observable<string>) => Observable<any>;
+    public games: Game[];
+    private typeAheadForPlayoffsUser: (text$: Observable<string>) => Observable<User[]>;
     private typeAheadInputFormatter: (value: User) => string;
     private typeAheadResultFormatter: (value: User) => string;
 
-    public constructor(private requestService: RequestService, private configurationService: ConfigurationService) {
-        this.typeAheadForGroupMember = (text$: Observable<string>) =>
+    public constructor(private requestService: RequestService, private configurationService: ConfigurationService,
+                       private standingsOrderPipe: StandingsOrderPipe) {
+        this.typeAheadForPlayoffsUser = (text$: Observable<string>) =>
             text$
                 .distinctUntilChanged()
-                .map(term => term);
+                .map(term => this.groups
+                    .map(g => this.standingsOrderPipe.transform(g.standings).slice(0, this.numberOfGroupMembersAdvancing))
+                    .reduce((previousStanding, currentStanding) => previousStanding.concat(currentStanding))
+                    .map(s => s.user)
+                    .filter(u => !this.games
+                        .find(g => (g.homeUser && g.homeUser.id) === u.id || (g.awayUser && g.awayUser.id === u.id)))
+                    .filter(u => u.username.toLowerCase().indexOf(term.toLowerCase()) !== -1));
         this.typeAheadInputFormatter = (value: User) => value.username;
         this.typeAheadResultFormatter = (value: User) => value.username;
     }
 
-    public get firstBranchGames(): GameDto[] {
+    public get firstBranchGames(): Game[] {
         return this.games.filter((value, index) => value && index < this.games.length / 2);
     }
 
-    public get secondBranchGames(): GameDto[] {
+    public get secondBranchGames(): Game[] {
         return this.games.filter((value, index) => value && index >= this.games.length / 2);
     }
 
@@ -59,7 +67,7 @@ export class AdminPlayoffsStartComponent implements OnInit {
 
                     let i;
                     for (i = 0; i < numberOfPlayoffSpots; i++) {
-                        this.games.push(<GameDto> {
+                        this.games.push(<Game> {
                             playoff: {
                                 spot: i + 1,
                                 round: 1
