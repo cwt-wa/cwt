488d5e9d12ac077fc39c06a778cd4dbcd4d9fc28
Zemke <flzemke@gmail.com>
2017-10-22 13:07:13 +0200

    Clean vendor.ts

--- cwt-angular/src/main/webapp/vendor.ts
+++ cwt-angular/src/main/webapp/vendor.ts
@@ -1,20 +1,14 @@
-// import "jquery/dist/jquery";
-// import "tether";
 import "@angular/platform-browser";
 import "@angular/platform-browser-dynamic";
 import "@angular/core";
 import "@angular/common";
 import "@angular/router";
-// import 'bootstrap/dist/js/bootstrap';
-// import 'bootstrap/dist/css/bootstrap.css';
+
 import '@ng-bootstrap/ng-bootstrap';
+
 import 'font-awesome/css/font-awesome.css';
-import 'toastr/toastr.js';
-import 'toastr/build/toastr.css';
+
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/observable/forkJoin';
 import 'rxjs/add/observable/throw';
-
-// Other vendors for example jQuery, Lodash or Bootstrap
-// You can import js, ts, css, sass, ...
