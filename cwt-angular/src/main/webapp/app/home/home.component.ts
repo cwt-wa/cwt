da547954293f993f41d65a97c559e7c5b297bfd6
Zemke <flzemke@gmail.com>
2017-10-22 18:05:27 +0200

    Display front page admin news

--- cwt-angular/src/main/webapp/app/home/home.component.ts
+++ cwt-angular/src/main/webapp/app/home/home.component.ts
@@ -1,19 +1,24 @@
 import {Component, OnInit} from "@angular/core";
 import {AuthService} from "../_services/auth.service";
-import {JwtUser} from "../custom";
+import {Configuration, JwtUser} from "../custom";
+import {ConfigurationService} from "../_services/configuration.service";
 
 @Component({
     selector: 'cwt-home',
     template: require('./home.component.html')
 })
 export class HomeComponent implements OnInit {
+    news: Configuration<string>;
     private authenticatedUser: JwtUser;
 
-    constructor(private authService: AuthService) {
+    constructor(private authService: AuthService, private configurationService: ConfigurationService) {
     }
 
     public ngOnInit(): void {
         this.authenticatedUser = this.authService.getUserFromTokenPayload();
+
+        this.configurationService.requestByKeys<string>("NEWS")
+            .subscribe(res => this.news = res[0]);
     }
 
 }
