0abfd8e7884c586dca83ef736b67754a46ccfe97
Zemke <flzemke@gmail.com>
2018-09-04 21:42:55 +0200

    GH-103 Add automatic draw logic and submit
    Closes GH-103.

--- cwt-angular/src/main/webapp/app/group/admin-groups-start-automatic-draw.component.ts
+++ cwt-angular/src/main/webapp/app/group/admin-groups-start-automatic-draw.component.ts
@@ -1,8 +1,12 @@
 import {Component, Inject, Input, OnInit} from "@angular/core";
-import {Application, Group, User} from "../custom";
+import {Application, Group, GroupDto, User} from "../custom";
 import {APP_CONFIG, AppConfig} from "../app.config";
 import {Observable} from "rxjs/Observable";
 import {distinctUntilChanged} from "rxjs/operators";
+import {RequestService} from "../_services/request.service";
+import {Router} from "@angular/router";
+
+const toastr = require('toastr/toastr.js');
 
 @Component({
     selector: 'cwt-admin-groups-start-automatic-draw',
@@ -24,7 +28,8 @@ export class AdminGroupsStartAutomaticDrawComponent implements OnInit {
     typeAheadInputFormatter: (value: User) => string;
     typeAheadResultFormatter: (value: User) => string;
 
-    constructor(@Inject(APP_CONFIG) private appConfig: AppConfig) {
+    constructor(@Inject(APP_CONFIG) private appConfig: AppConfig, private requestService: RequestService,
+                private router: Router) {
         this.numberOfGroups = this.appConfig.tournament.numberOfGroups;
         this.usersPerGroup = this.appConfig.tournament.usersPerGroup;
 
@@ -38,6 +43,36 @@ export class AdminGroupsStartAutomaticDrawComponent implements OnInit {
                         .filter(a => a.username.toLowerCase().indexOf(term.toLowerCase()) !== -1));
         this.typeAheadInputFormatter = (value: User) => value.username || null;
         this.typeAheadResultFormatter = (value: User) => value.username;
+    }
+
+    public submit(): void {
+        const randomlyDrawnGroups: GroupDto[] = this.groups.map(g => ({label: g.label, users: []}));
+
+        let i;
+        for (i = 0; i < this.appConfig.tournament.usersPerGroup; i++) {
+            const indicesOfGroupsAlreadyDrawnAUserOfCurrentPot = [];
+
+            let j;
+            for (j = 0; j < this.appConfig.tournament.numberOfGroups; j++) {
+                const user = this.pots[i][j];
+
+                let randomIndexOfGroup: number;
+                do {
+                    randomIndexOfGroup = Math.floor(Math.random() * this.appConfig.tournament.numberOfGroups);
+                } while (indicesOfGroupsAlreadyDrawnAUserOfCurrentPot.indexOf(randomIndexOfGroup) !== -1);
+
+                randomlyDrawnGroups[randomIndexOfGroup].users.push(user.id);
+                indicesOfGroupsAlreadyDrawnAUserOfCurrentPot.push(randomIndexOfGroup)
+            }
+        }
+
+        this.requestService.post('tournament/current/group/many', randomlyDrawnGroups)
+            .subscribe(
+                () => {
+                    this.router.navigateByUrl('/applicants');
+                    toastr.success("Successfully saved.");
+                },
+                () => toastr.error("An unknown error occurred."));
     }
 
     public get drawnApplicants(): User[] {
