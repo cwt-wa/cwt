0a101dd1279a43aeffca4a326c964606f15d64ba
Zemke <flzemke@gmail.com>
2019-01-20 22:59:32 +0100

    GH-150 Migrate GameDetailDto to GameCreationDto

--- cwt-angular/src/main/webapp/app/playoffs/admin-playoffs-start.component.ts
+++ cwt-angular/src/main/webapp/app/playoffs/admin-playoffs-start.component.ts
@@ -3,7 +3,7 @@ import {distinctUntilChanged} from 'rxjs/operators';
 
 import {Component, OnInit} from '@angular/core';
 import {RequestService} from "../_services/request.service";
-import {Configuration, GameCreationDto, GameDetailDto, Group, User} from "../custom";
+import {Configuration, GameCreationDto, Group} from "../custom";
 import {ConfigurationService} from "../_services/configuration.service";
 import {Observable} from "rxjs/Observable";
 import {StandingsOrderPipe} from "../_util/standings-order.pipe";
@@ -16,25 +16,29 @@ export class AdminPlayoffsStartComponent implements OnInit {
 
     public groups: Group[];
     public numberOfGroupMembersAdvancing: number;
-    public games: GameDetailDto[];
-    typeAheadForPlayoffsUser: (text$: Observable<string>) => Observable<User[]>;
-    typeAheadInputFormatter: (value: User) => string;
-    typeAheadResultFormatter: (value: User) => string;
+    public games: GameCreationDto[];
+    typeAheadForPlayoffsUser: (text$: Observable<string>) => Observable<number[]>;
+    typeAheadInputFormatter: (userId: number) => string;
+    typeAheadResultFormatter: (userId: number) => string;
 
     public constructor(private requestService: RequestService, private configurationService: ConfigurationService,
                        private standingsOrderPipe: StandingsOrderPipe) {
         this.typeAheadForPlayoffsUser = (text$: Observable<string>) =>
             text$
                 .pipe(distinctUntilChanged())
-                .map(term => this.groups
-                    .map(g => this.standingsOrderPipe.transform(g.standings).slice(0, this.numberOfGroupMembersAdvancing))
-                    .reduce((previousStanding, currentStanding) => previousStanding.concat(currentStanding))
-                    .map(s => s.user)
-                    .filter(u => !this.games
-                        .find(g => (g.homeUser && g.homeUser.id) === u.id || (g.awayUser && g.awayUser.id === u.id)))
-                    .filter(u => u.username.toLowerCase().indexOf(term.toLowerCase()) !== -1));
-        this.typeAheadInputFormatter = (value: User) => value.username;
-        this.typeAheadResultFormatter = (value: User) => value.username;
+                .map(term => this.getPlayoffUsers()
+                    .filter(u => !this.games.find(g => g.homeUser === u.id || g.awayUser === u.id))
+                    .filter(u => u.username.toLowerCase().indexOf(term.toLowerCase()) !== -1)
+                    .map(u => u.id));
+        this.typeAheadInputFormatter = (userId: number) => this.getPlayoffUsers().find(u => u.id === userId).username;
+        this.typeAheadResultFormatter = (userId: number) => this.getPlayoffUsers().find(u => u.id === userId).username;
+    }
+
+    private getPlayoffUsers() {
+        return this.groups
+            .map(g => this.standingsOrderPipe.transform(g.standings).slice(0, this.numberOfGroupMembersAdvancing))
+            .reduce((previousStanding, currentStanding) => previousStanding.concat(currentStanding))
+            .map(s => s.user);
     }
 
     public get numberOfGroupMembersAdvancingIterable(): number[] {
@@ -62,12 +66,12 @@ export class AdminPlayoffsStartComponent implements OnInit {
 
                     let i;
                     for (i = 0; i < numberOfPlayoffSpots; i++) {
-                        this.games.push(<GameDetailDto> {
+                        this.games.push({
                             playoff: {
                                 spot: i + 1,
                                 round: 1
                             }
-                        });
+                        } as GameCreationDto);
                     }
                 }
             );
@@ -76,19 +80,12 @@ export class AdminPlayoffsStartComponent implements OnInit {
     public isDrawn(userId: number): boolean {
         return this.games
             .find(
-                g => (g.awayUser != null && g.awayUser.id === userId)
-                    || (g.homeUser != null && g.homeUser.id === userId)) != null;
+                g => (g.awayUser != null && g.awayUser === userId)
+                    || (g.homeUser != null && g.homeUser === userId)) != null;
     }
 
     public submit(): void {
-        const body: GameCreationDto[] = this.games
-            .map(g => <GameCreationDto> {
-                homeUser: g.homeUser.id,
-                awayUser: g.awayUser.id,
-                playoff: g.playoff
-            });
-
-        this.requestService.post('game/many', body)
+        this.requestService.post<GameCreationDto[]>('game/many', this.games)
             .subscribe();
     }
 }
