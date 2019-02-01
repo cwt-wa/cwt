aa39d2822385691a4b5a1e5c5dc7933e7c8be939
Zemke <flzemke@gmail.com>
2019-02-01 20:58:46 +0100

    Migrate CwtApplicationTests to Kotlin

---
+++ cwt-spring/src/test/java/com/cwtsite/cwt/CwtApplicationTests.kt
@@ -0,0 +1,24 @@
+package com.cwtsite.cwt
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(properties = [
+    "spring.datasource.url=jdbc:postgresql://127.0.0.1:5433/postgres",
+    "spring.flyway.locations=classpath:db/migration/common,classpath:db/migration/test"
+])
+class CwtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
