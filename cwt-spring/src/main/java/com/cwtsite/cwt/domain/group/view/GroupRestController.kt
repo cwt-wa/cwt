4cdc38fd2cd65be8fe20c733b2947d55a1866cea
Zemke <flzemke@gmail.com>
2019-02-09 12:18:12 +0100

    GH-133 Kotlinify GroupRestController

---
+++ cwt-spring/src/main/java/com/cwtsite/cwt/domain/group/view/GroupRestController.kt
@@ -0,0 +1,8 @@
+package com.cwtsite.cwt.domain.group.view
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/group")
+class GroupRestController
