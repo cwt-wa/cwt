ba2860a74b207b5a874d3662b2109ef7a870e285
Zemke <flzemke@gmail.com>
2019-02-10 14:04:06 +0100

    GH-131 Kotlinify UserRepository

---
+++ cwt-spring/src/main/java/com/cwtsite/cwt/domain/user/repository/UserRepository.kt
@@ -0,0 +1,11 @@
+package com.cwtsite.cwt.domain.user.repository
+
+import com.cwtsite.cwt.domain.user.repository.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : JpaRepository<User, Long> {
+    fun findByUsername(username: String): User
+    fun findByEmailEqualsOrUsernameEquals(email: String, username: String): User
+}
