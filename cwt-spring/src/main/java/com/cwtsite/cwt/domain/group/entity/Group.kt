ae15080c0d2e66c855586261396e0b162c04f945
Zemke <flzemke@gmail.com>
2019-03-03 20:49:40 +0100

    Kotlinify Group

---
+++ cwt-spring/src/main/java/com/cwtsite/cwt/domain/group/entity/Group.kt
@@ -0,0 +1,30 @@
+package com.cwtsite.cwt.domain.group.entity
+
+import com.cwtsite.cwt.domain.group.entity.enumeration.GroupLabel
+import com.cwtsite.cwt.domain.tournament.entity.Tournament
+import com.cwtsite.cwt.entity.GroupStanding
+import javax.persistence.*
+
+@Entity
+@Table(name = "\"group\"")
+@SequenceGenerator(name = "group_seq", sequenceName = "group_seq", initialValue = 121, allocationSize = 1)
+data class Group(
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "group_seq")
+        var id: Long? = null,
+
+        @Enumerated(EnumType.STRING)
+        @Column(name = "label")
+        var label: GroupLabel? = null,
+
+        @ManyToOne
+        var tournament: Tournament? = null,
+
+        @OneToMany(cascade = [CascadeType.ALL])
+        @JoinColumn(name = "group_id")
+        val standings: MutableList<GroupStanding> = mutableListOf()
+
+//        @OneToMany(cascade = [CascadeType.ALL])
+//        val games: MutableList<Game> = mutableListOf()
+)
