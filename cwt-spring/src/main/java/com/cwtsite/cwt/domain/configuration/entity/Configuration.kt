44cbe8086689e6f53e355bf4ec0253e86db42f60
Zemke <flzemke@gmail.com>
2019-03-11 21:24:59 +0100

    GH-137 Kotlinify Configuration

---
+++ cwt-spring/src/main/java/com/cwtsite/cwt/domain/configuration/entity/Configuration.kt
@@ -0,0 +1,28 @@
+package com.cwtsite.cwt.domain.configuration.entity
+
+
+import com.cwtsite.cwt.domain.configuration.entity.enumeratuion.ConfigurationKey
+import com.cwtsite.cwt.domain.user.repository.entity.User
+import org.hibernate.annotations.UpdateTimestamp
+import java.sql.Timestamp
+import javax.persistence.*
+
+@Entity
+@Table(name = "configuration")
+data class Configuration(
+
+        @Id
+        @Column(name = "key")
+        @Enumerated(EnumType.STRING)
+        var key: ConfigurationKey? = null,
+
+        @Column(name = "value", columnDefinition = "text")
+        var value: String? = null,
+
+        @Column(name = "modified")
+        @field:UpdateTimestamp
+        var modified: Timestamp? = null,
+
+        @ManyToOne
+        var author: User? = null
+)
