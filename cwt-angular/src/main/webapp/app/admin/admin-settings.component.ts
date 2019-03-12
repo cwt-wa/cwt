73bae9d28ae8a2a36c3a48629d389034f8320b37
Zemke <flzemke@gmail.com>
2019-03-12 19:14:33 +0100

    GH-137 Submit settings change

--- cwt-angular/src/main/webapp/app/admin/admin-settings.component.ts
+++ cwt-angular/src/main/webapp/app/admin/admin-settings.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {Configuration} from "../custom";
+import {Configuration, ConfigurationKey} from "../custom";
 import {ConfigurationService} from "../_services/configuration.service";
+import {RequestService} from "../_services/request.service";
+
+const toastr = require('toastr/toastr.js');
 
 @Component({
     selector: 'cwt-admin-settings',
@@ -11,11 +14,16 @@ export class AdminSettingsComponent implements OnInit {
 
     configurations: Configuration[];
 
-    constructor(private configurationService: ConfigurationService) {
+    constructor(private configurationService: ConfigurationService, private requestService: RequestService) {
     }
 
     ngOnInit(): void {
         this.configurationService.request()
             .subscribe(res => this.configurations = res.filter(c => c.key !== 'NEWS' && c.key !== 'RULES'));
+    }
+
+    submit(key: ConfigurationKey, value: string) {
+        this.requestService.post("configuration", {key, value})
+            .subscribe(() => toastr.success("Successfully saved."))
     }
 }
