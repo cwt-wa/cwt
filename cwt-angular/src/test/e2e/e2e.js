7bb5068d4093c0838f8ae0160d7f0c90a4f8cde1
Zemke <flzemke@gmail.com>
2019-05-26 21:14:31 +0200

    GH-148 Allow for use of `browser.pause()` without timing out

--- cwt-angular/src/test/e2e/e2e.js
+++ cwt-angular/src/test/e2e/e2e.js
@@ -1,4 +1,11 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['playoff-tree.spec.js']
+    specs: ['playoff-tree.spec.js'],
+    allScriptsTimeout: 999999,
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 999999
+    },
+    onPrepare: function () {
+        browser.manage().window().maximize();
+    }
 };
