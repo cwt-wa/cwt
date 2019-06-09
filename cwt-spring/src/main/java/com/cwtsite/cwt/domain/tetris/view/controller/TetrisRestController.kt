d347d882e83f82d8987bd57c13d2992a2def75eb
lebuenti <leabuenti@gmail.com>
2019-06-09 16:44:08 +0200

    Create Tetris Rest Controller
    and insert function for save a tetris entry without user

---
+++ cwt-spring/src/main/java/com/cwtsite/cwt/domain/tetris/view/controller/TetrisRestController.kt
@@ -0,0 +1,24 @@
+package com.cwtsite.cwt.domain.tetris.view.controller
+
+import com.cwtsite.cwt.domain.tetris.service.TetrisService
+import com.cwtsite.cwt.domain.tetris.view.model.TetrisDto
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+import java.sql.Timestamp
+
+@RestController
+@RequestMapping("/api/tetris")
+class TetrisRestController @Autowired
+constructor(private val tetrisService: TetrisService) {
+
+    @RequestMapping("", method = [RequestMethod.POST])
+    fun saveTetris(@RequestBody highscore: Long): ResponseEntity<TetrisDto> {
+        val timestamp = Timestamp(System.currentTimeMillis());
+        return ResponseEntity.ok(TetrisDto.toDto(tetrisService.add(null, highscore, timestamp)))
+    }
+
+}
