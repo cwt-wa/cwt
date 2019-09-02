b9574c7cd6af2e101f093a56731520cbef435e9c
Zemke <florian@zemke.io>
2019-09-03 00:27:37 +0200

    GH-113 Min length for domains

--- cwt-angular/src/main/webapp/app/_util/convert-links.pipe.ts
+++ cwt-angular/src/main/webapp/app/_util/convert-links.pipe.ts
@@ -13,7 +13,7 @@ export class ConvertLinksPipe implements PipeTransform {
 
         const htmlClasses = classes.length ? ` class="${classes.join(" ")}" ` : " ";
 
-        const regexWithProtocol = /((?:http[s]?:\/\/)?(?:www)?[.a-z0-9-]+?\.(?:[a-z]+)(?:[.a-z0-9-\/]+)?(?:\?\S*)?)/gi;
+        const regexWithProtocol = /((?:http[s]?:\/\/)?(?:www)?[.a-z0-9-]{3,}?\.(?:[a-z]{2,})(?:[.a-z0-9-\/]+)?(?:\?\S*)?)/gi;
         value = value.replace(regexWithProtocol, `<a href="http://$1"${htmlClasses}target="_blank">$1</a>`);
         value = value.replace(/http?:\/\/(http[s]?:\/\/)/gi, '$1');
         return value;
