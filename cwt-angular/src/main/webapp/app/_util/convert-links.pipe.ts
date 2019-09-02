bb3b0298994321d3b53e611676968a75862cff7b
Zemke <florian@zemke.io>
2019-09-03 01:06:12 +0200

    GH-113 Word boundaries

--- cwt-angular/src/main/webapp/app/_util/convert-links.pipe.ts
+++ cwt-angular/src/main/webapp/app/_util/convert-links.pipe.ts
@@ -13,7 +13,7 @@ export class ConvertLinksPipe implements PipeTransform {
 
         const htmlClasses = classes.length ? ` class="${classes.join(" ")}" ` : " ";
 
-        const regexWithProtocol = /((?:http[s]?:\/\/)?(?:www)?[a-z0-9-]{3,}?\.(?:[a-z]{2,})(?:[.a-z0-9-\/]+)?(?:[^\s]*)?)/gi;
+        const regexWithProtocol = /(\b(?:http[s]?:\/\/)?(?:www)?[a-z0-9-]{3,}?\.(?:[a-z]{2,})(?:[.a-z0-9-\/]+)?(?:[^\s]*)?\b)/gi;
         value = value.replace(regexWithProtocol, `<a href="http://$1"${htmlClasses}target="_blank">$1</a>`);
         value = value.replace(/http?:\/\/(http[s]?:\/\/)/gi, '$1');
         return value;
