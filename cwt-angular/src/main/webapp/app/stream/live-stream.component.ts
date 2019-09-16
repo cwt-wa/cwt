95a632c8b56dff2c07168ca4c6c4b92be19e2351
Zemke <florian@zemke.io>
2019-09-16 18:13:25 +0200

    GH-166 Whitespace changes after vim journey

--- cwt-angular/src/main/webapp/app/stream/live-stream.component.ts
+++ cwt-angular/src/main/webapp/app/stream/live-stream.component.ts
@@ -1,44 +1,46 @@
-import {Component, OnInit, OnDestroy, Inject} from '@angular/core';
+import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
 import {RequestService} from '../_services/request.service';
 import {ChannelDto} from '../custom';
-import {AppConfig, APP_CONFIG} from '../app.config';
+import {APP_CONFIG, AppConfig} from '../app.config';
 
 interface LiveStream {
     event_id: string;
     user_id: string;
     user_name: string;
     title: string;
-};
+}
 
 @Component({
     selector: 'cwt-live-stream',
     template: `
-<div *ngFor="let stream of liveStreams" class="alert alert-info">
-    <div class="d-flex justify-content-between align-items-center">
-        <div class="d-flex align-items-center">
-            <i class="fa fa-twitch fa-lg mr-2"></i>
-            <span>
+        <div *ngFor="let stream of liveStreams" class="alert alert-info">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="d-flex align-items-center">
+                    <i class="fa fa-twitch fa-lg mr-2"></i>
+                    <span>
                 <strong>{{userIdByChannelName[stream.user_id] || stream.user_name}}</strong>
                 is live with
                 <strong>{{stream.title}}</strong>
             </span>
+                </div>
+                <a target="_blank" [href]="'https://twitch.tv/' + stream.user_name"
+                   class="btn btn-sm btn-primary float-right">
+                    Watch
+                </a>
+            </div>
         </div>
-        <a target="_blank" [href]="'https://twitch.tv/' + stream.user_name"
-           class="btn btn-sm btn-primary float-right">
-            Watch
-        </a>
-    </div>
-</div>
-`
+    `
 })
 export class LiveStreamComponent implements OnInit, OnDestroy {
 
-    private eventSource: EventSource;
     liveStreams: LiveStream[];
-    userIdByChannelName: {[key: string]: string} = {} as any;
+    userIdByChannelName: { [key: string]: string } = {} as any;
+
+    private eventSource: EventSource;
 
     constructor(private requestService: RequestService,
-                @Inject(APP_CONFIG) private appConfig: AppConfig) {}
+                @Inject(APP_CONFIG) private appConfig: AppConfig) {
+    }
 
     ngOnInit(): void {
         this.eventSource = new EventSource(this.appConfig.liveStreamProducer);
@@ -47,11 +49,10 @@ export class LiveStreamComponent implements OnInit, OnDestroy {
             this.liveStreams = JSON.parse(e.data);
 
             if (!!this.liveStreams.length
-                    && !Object.keys(this.userIdByChannelName).length) {
+                && !Object.keys(this.userIdByChannelName).length) {
                 this.queryChannels();
             }
         };
-
     }
 
     ngOnDestroy() {
@@ -63,6 +64,6 @@ export class LiveStreamComponent implements OnInit, OnDestroy {
             .subscribe(res => this.userIdByChannelName = res.reduce((prev, curr) => {
                 prev[curr.id] = curr.displayName;
                 return prev;
-            }, {} as {[key: string]: string}));
+            }, {} as { [key: string]: string }));
     }
 }
