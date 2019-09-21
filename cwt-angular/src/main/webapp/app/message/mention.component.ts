9398f588627aedeb40c96273fe234866cc4a5402
Zemke <florian@zemke.io>
2019-09-21 16:43:45 +0200

    GH-188 Complete on blur

--- cwt-angular/src/main/webapp/app/message/mention.component.ts
+++ cwt-angular/src/main/webapp/app/message/mention.component.ts
@@ -2,6 +2,7 @@ import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
 import {Component, ElementRef, EventEmitter, ViewChild} from '@angular/core';
 import {User} from "../custom";
 import {Observable} from "rxjs";
+import {NgbTypeahead} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
     selector: 'mention',
@@ -38,13 +39,14 @@ export class MentionComponent {
         text$
             .pipe(debounceTime(200))
             .pipe(distinctUntilChanged()).pipe(
-            map(term => this.suggestedUsers.filter(u => u.username.toLowerCase().indexOf(term.toLowerCase()) > -1)));
+            map(this.filterByTerm.bind(this)));
     typeAheadInputFormatter = (value: User) => value.username || null;
     typeAheadResultFormatter = (value: User) => value.username || null;
     disabled: boolean;
     mentionHasJustBeenSelected: boolean;
-    @ViewChild('nameInput')
-    private nameInput: ElementRef;
+
+    @ViewChild('nameInput') private nameInput: ElementRef;
+    @ViewChild('ngbTypeaheadInstance') private ngbTypeaheadInstance: NgbTypeahead;
 
     constructor(private elementRef: ElementRef) {
     }
@@ -53,17 +55,36 @@ export class MentionComponent {
         (this.nameInput.nativeElement as HTMLInputElement).focus();
     }
 
+    public filterByTerm(term: string = this.nameInput.nativeElement.value) {
+        return this.suggestedUsers.filter(
+            u => u.username.toLowerCase().indexOf(term.toLowerCase()) > -1);
+    }
+
     public onSelectTypeAheadSuggestionItem(): void {
         this.mentionHasJustBeenSelected = true;
         this.putCursorAfterMention();
     }
 
     public keyDown($event: KeyboardEvent): void {
-        if ($event.key !== 'Backspace') {
-            return;
+        if ($event.key === 'Backspace') {
+            const term = this.nameInput.nativeElement.value;
+            if (!this.mentionedUser && term.length === 0) {
+                this.removeMention.emit();
+            }
+        } else if ($event.key === 'Escape') {
+            this.removeMention.emit();
+        } else if ($event.key === ' ') {
+            ($event.target as HTMLInputElement).blur();
         }
+    }
 
-        if (!this.mentionedUser) {
+    public onBlur() {
+        this.ngbTypeaheadInstance.dismissPopup();
+        const possibleUsers = this.filterByTerm();
+        if (possibleUsers.length === 1) {
+            this.mentionedUser = possibleUsers[0];
+            this.onSelectTypeAheadSuggestionItem();
+        } else {
             this.removeMention.emit();
         }
     }
