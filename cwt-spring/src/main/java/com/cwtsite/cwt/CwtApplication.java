fa4aac8d930cd15cf8da4a0febf931481bddbd11
Zemke <florian@zemke.io>
2019-10-01 18:57:41 +0200

    GH-174 Add mail sending capability

--- cwt-spring/src/main/java/com/cwtsite/cwt/CwtApplication.java
+++ cwt-spring/src/main/java/com/cwtsite/cwt/CwtApplication.java
@@ -2,9 +2,12 @@ package com.cwtsite.cwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableConfigurationProperties(MailProperties.class)
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class CwtApplication {
 
--- cwt-spring/src/main/java/com/cwtsite/cwt/security/WebSecurityConfigurer.java
+++ cwt-spring/src/main/java/com/cwtsite/cwt/security/WebSecurityConfigurer.java
@@ -5,9 +5,12 @@ import com.cwtsite.cwt.security.filter.CorsFilter;
 import com.cwtsite.cwt.security.filter.JwtAuthenticationTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -21,24 +24,30 @@ import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
+import java.util.Properties;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
 
     private final JwtAuthenticationEntryPoint unauthorizedHandler;
     private final UserDetailsService userDetailsService;
 
     private final AuthenticationProvider progressiveAuthenticationProvider;
 
+    private final MailProperties mailProperties;
+
     @Value("${config.logging.max-payload-size}")
     private Integer maxPayloadSize;
 
     @Autowired
     public WebSecurityConfigurer(JwtAuthenticationEntryPoint unauthorizedHandler, UserDetailsService userDetailsService,
-                                 AuthenticationProvider progressiveAuthenticationProvider) {
+                                 AuthenticationProvider progressiveAuthenticationProvider, MailProperties mailProperties) {
         this.unauthorizedHandler = unauthorizedHandler;
         this.userDetailsService = userDetailsService;
         this.progressiveAuthenticationProvider = progressiveAuthenticationProvider;
+        this.mailProperties = mailProperties;
     }
 
     @Override
@@ -73,6 +82,22 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
         commonsRequestLoggingFilter.setAfterMessageSuffix("");
 
         return commonsRequestLoggingFilter;
+    }
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(mailProperties.getHost());
+        mailSender.setPort(mailProperties.getPort());
+        mailSender.setUsername(mailProperties.getUsername());
+        mailSender.setPassword(mailProperties.getPassword());
+        mailSender.setProtocol(mailProperties.getProtocol());
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.putAll(mailProperties.getProperties());
+
+        return mailSender;
     }
 
     @Override
