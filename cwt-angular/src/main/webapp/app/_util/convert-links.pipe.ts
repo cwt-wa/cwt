dffa21bb0d46ad26defba76753fd04f37e51600f
Zemke <florian@zemke.io>
2019-12-15 11:58:57 +0100

    Fallback on incompatible regex

--- cwt-angular/src/main/webapp/app/_util/convert-links.pipe.ts
+++ cwt-angular/src/main/webapp/app/_util/convert-links.pipe.ts
@@ -13,12 +13,20 @@ export class ConvertLinksPipe implements PipeTransform {
 
         const htmlClasses = classes.length ? ` class="${classes.join(" ")}" ` : " ";
 
-        const regexWithProtocol = /(?<!@)(\b(?:http[s]?:\/\/)?(?:www\.)?[a-z0-9-]{3,}?\.(?:[a-z]{2,})(?:[.a-z0-9-\/]+)?(?:[^\s]*)?\b)/gi;
-        value = value.replace(regexWithProtocol, `<a href="http://$1"${htmlClasses}target="_blank">$1</a>`);
-        value = value.replace(/http?:\/\/(http[s]?:\/\/)/gi, '$1');
+        try {
+            // Lookarounds are apparently only supported by Chrome.
+            let regexWithProtocol = new RegExp("(?<!@)(\\b(?:http[s]?:\\/\\/)?(?:www\\.)?[a-z0-9-]{3,}?\\.(?:[a-z]{2,})(?:[.a-z0-9-\\/]+)?(?:[^\\s]*)?\\b)", "gi");
+            value = value.replace(regexWithProtocol, `<a href="http://$1"${htmlClasses}target="_blank">$1</a>`);
+            value = value.replace(/http?:\/\/(http[s]?:\/\/)/gi, '$1');
 
-        const regexForMail = /(\b\S+@\S+\b)/g;
-        value = value.replace(regexForMail, '<a href="mailto:$1">$1</a>');
+            const regexForMail = /(\b\S+@\S+\b)/g;
+            value = value.replace(regexForMail, '<a href="mailto:$1">$1</a>');
+        } catch (e) {
+            let regexWithProtocol = /(\b(?:http[s]?:\/\/)?(?:www\.)?[a-z0-9-]{3,}?\.(?:[a-z]{2,})(?:[.a-z0-9-\/]+)?(?:[^\s]*)?\b)/gi;
+            value = value.replace(regexWithProtocol, `<a href="http://$1"${htmlClasses}target="_blank">$1</a>`);
+            value = value.replace(/http?:\/\/(http[s]?:\/\/)/gi, '$1');
+            return value;
+        }
 
         return value;
     }
