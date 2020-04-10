f7a5f72ed6e6fb894233403093f831a38eead271
Zemke <florian@zemke.io>
2020-04-10 18:05:36 +0200

    Implement all methods

--- cwt-spring/src/main/java/com/cwtsite/cwt/core/BinaryOutboundServiceDevImpl.kt
+++ cwt-spring/src/main/java/com/cwtsite/cwt/core/BinaryOutboundServiceDevImpl.kt
@@ -3,6 +3,7 @@ package com.cwtsite.cwt.core
 import com.cwtsite.cwt.core.profile.Dev
 import khttp.responses.Response
 import org.apache.http.HttpEntity
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
@@ -10,6 +11,12 @@ import java.io.File
 @Dev
 @Service
 class BinaryOutboundServiceDevImpl : BinaryOutboundService {
+
+    @Value("\${binary-data-store:#{null}}")
+    private var binaryDataStoreEndpoint: String? = null
+
+    @Value("\${waaas-endpoint:#{null}}")
+    private var waaasEndpoint: String? = null
 
     override fun retrieveUserPhoto(userId: Long): Response {
         throw UnsupportedOperationException("Not implemented")
@@ -34,4 +41,8 @@ class BinaryOutboundServiceDevImpl : BinaryOutboundService {
     override fun extractGameStats(gameId: Long, extractedReplay: File): HttpEntity {
         throw UnsupportedOperationException("Not implemented")
     }
+
+    override fun binaryDataStoreConfigured(): Boolean = binaryDataStoreEndpoint != null
+
+    override fun waaasConfigured(): Boolean = waaasEndpoint != null
 }
