739377c5a5ec6e21fdde1c6bfc9774d37393c527
Zemke <florian@zemke.io>
2020-04-10 20:05:40 +0200

    GH-221 Explicity stats extraction from replay upload

--- cwt-spring/src/test/java/com/cwtsite/cwt/integration/GameStatsWebIntegration.kt
+++ cwt-spring/src/test/java/com/cwtsite/cwt/integration/GameStatsWebIntegration.kt
@@ -6,6 +6,7 @@ import com.cwtsite.cwt.domain.game.service.GameRepository
 import com.cwtsite.cwt.domain.tournament.entity.Tournament
 import com.cwtsite.cwt.domain.tournament.service.TournamentRepository
 import com.cwtsite.cwt.domain.user.repository.UserRepository
+import com.cwtsite.cwt.domain.user.repository.entity.AuthorityRole
 import com.cwtsite.cwt.domain.user.repository.entity.User
 import com.cwtsite.cwt.domain.user.service.AuthService
 import com.cwtsite.cwt.test.MockitoUtils.anyObject
@@ -40,6 +41,9 @@ import java.time.Instant
 import kotlin.test.Test
 
 
+private const val game1Id = 1559
+private const val game2Id = 1513
+
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -70,11 +74,18 @@ class GameStatsWebIntegration {
 
     private val anyToken = "ANY_TOKEN"
 
-    private val zipArchive = File(javaClass.getResource("1559.zip").toURI())
-    private val statsJson1 = javaClass.getResourceAsStream("1.json")!!
-    private val statsJson2 = javaClass.getResourceAsStream("2.json")!!
-    private val statsJson3 = javaClass.getResourceAsStream("3.json")!!
-    private val statsJson4 = javaClass.getResourceAsStream("4.json")!!
+    private val game1ZipArchive = File(javaClass.getResource("$game1Id.zip").toURI())
+    private val game2ZipArchive = File(javaClass.getResource("$game2Id.zip").toURI())
+    private val game1StatsJson = listOf(
+            javaClass.getResourceAsStream("$game1Id/1.json")!!,
+            javaClass.getResourceAsStream("$game1Id/2.json")!!,
+            javaClass.getResourceAsStream("$game1Id/3.json")!!,
+            javaClass.getResourceAsStream("$game1Id/4.json")!!)
+    private val game2StatsJson = listOf(
+            javaClass.getResourceAsStream("$game2Id/1.json")!!,
+            javaClass.getResourceAsStream("$game2Id/2.json")!!,
+            javaClass.getResourceAsStream("$game2Id/3.json")!!,
+            javaClass.getResourceAsStream("$game2Id/4.json")!!)
 
     companion object {
 
@@ -85,38 +96,49 @@ class GameStatsWebIntegration {
         private var rafkaUser: User? = null
 
         @JvmStatic
-        private var game: Game? = null
+        private var game1: Game? = null
+
+        @JvmStatic
+        private var game2: Game? = null
     }
 
     @Before
     fun setUp() {
-        if (game == null) {
-            game = gameRepository.save(Game(
+        if (game1 == null) {
+            game1 = gameRepository.save(Game(
+                    tournament = tournamentRepository.save(
+                            Tournament(created = Timestamp.from(Instant.now())))))
+        }
+        if (game2 == null) {
+            game2 = gameRepository.save(Game(
                     tournament = tournamentRepository.save(
                             Tournament(created = Timestamp.from(Instant.now())))))
         }
         if (zemkeUser == null) zemkeUser = userRepository.save(User(username = "nOox", email = "nOox@cwtsite.com"))
         if (rafkaUser == null) rafkaUser = userRepository.save(User(username = "Boolc", email = "Boolc@cwtsite.com"))
+
         `when`(authService.tokenHeaderName).thenReturn(tokenHeader)
         `when`(authService.getUserFromToken(anyToken)).thenReturn(zemkeUser)
+
+        `when`(binaryOutboundService.binaryDataStoreConfigured()).thenReturn(true)
+        `when`(binaryOutboundService.waaasConfigured()).thenReturn(true)
     }
 
     @Test
     @WithMockUser
-    fun `1 save game stats json`() {
-        `when`(binaryOutboundService.binaryDataStoreConfigured()).thenReturn(true)
-        `when`(binaryOutboundService.waaasConfigured()).thenReturn(true)
-
+    fun `1 save replay file`() {
         `when`(binaryOutboundService.extractGameStats(anyLong(), anyObject()))
-                .thenAnswer { InputStreamEntity(statsJson1) }
-                .thenAnswer { InputStreamEntity(statsJson2) }
-                .thenAnswer { InputStreamEntity(statsJson3) }
-                .thenAnswer { InputStreamEntity(statsJson4) }
+                .thenAnswer { InputStreamEntity(game1StatsJson[0]) }
+                .thenAnswer { InputStreamEntity(game1StatsJson[1]) }
+                .thenAnswer { InputStreamEntity(game1StatsJson[2]) }
+                .thenAnswer { InputStreamEntity(game1StatsJson[3]) }
 
         mockMvc
-                .perform(multipart("/api/binary/game/${game!!.id}/replay")
-                        .file(MockMultipartFile("replay", zipArchive.name, "application/zip", FileInputStream(zipArchive)))
+                .perform(multipart("/api/binary/game/${game1!!.id}/replay")
+                        .file(MockMultipartFile("replay", game1ZipArchive.name, "application/zip", FileInputStream(game1ZipArchive)))
                         .header(tokenHeader, anyToken)
+                        .param("score-home", "3")
+                        .param("score-away", "1")
                         .param("home-user", zemkeUser!!.id.toString())
                         .param("away-user", rafkaUser!!.id.toString())
                 )
@@ -127,7 +149,7 @@ class GameStatsWebIntegration {
     @Test
     fun `2 retrieve game stats json`() {
         mockMvc
-                .perform(get("/api/game/${game!!.id}/stats")
+                .perform(get("/api/game/${game1!!.id}/stats")
                         .contentType(APPLICATION_JSON_UTF8))
                 .andDo(print())
                 .andExpect(status().isOk)
@@ -140,5 +162,35 @@ class GameStatsWebIntegration {
                 .andExpect(jsonPath("$[1].startedAt", `is`("2019-10-06 12:26:30 GMT")))
                 .andExpect(jsonPath("$[2].startedAt", `is`("2019-10-06 12:45:24 GMT")))
                 .andExpect(jsonPath("$[3].startedAt", `is`("2019-10-06 13:10:39 GMT")))
+    }
+
+    @Test
+    @WithMockUser(authorities = [AuthorityRole.ROLE_ADMIN])
+    fun `3 save game stats explicitly`() {
+        `when`(binaryOutboundService.extractGameStats(anyLong(), anyObject()))
+                .thenAnswer { InputStreamEntity(game2StatsJson[0]) }
+                .thenAnswer { InputStreamEntity(game2StatsJson[1]) }
+                .thenAnswer { InputStreamEntity(game2StatsJson[2]) }
+                .thenAnswer { InputStreamEntity(game2StatsJson[3]) }
+
+        mockMvc
+                .perform(multipart("/api/binary/game/${game2!!.id}/stats")
+                        .file(MockMultipartFile("replay", game2ZipArchive.name, "application/zip", FileInputStream(game2ZipArchive)))
+                        .header(tokenHeader, anyToken)
+                        .param("home-user", zemkeUser!!.id.toString())
+                        .param("away-user", rafkaUser!!.id.toString())
+                )
+                .andDo(print())
+                .andExpect(status().isCreated)
+                .andExpect(jsonPath("$", hasSize<Any>(4)))
+                .andExpect(jsonPath("$[0].gameId", `is`("10504579")))
+                .andExpect(jsonPath("$[1].gameId", `is`("10504629")))
+                .andExpect(jsonPath("$[2].gameId", `is`("10505983")))
+                .andExpect(jsonPath("$[3].gameId", `is`("10505983")))
+                .andExpect(jsonPath("$[0].startedAt", `is`("2019-09-16 16:58:33 GMT")))
+                .andExpect(jsonPath("$[1].startedAt", `is`("2019-09-16 17:31:45 GMT")))
+                .andExpect(jsonPath("$[2].startedAt", `is`("2019-09-17 16:44:46 GMT")))
+                .andExpect(jsonPath("$[3].startedAt", `is`("2019-09-17 17:02:45 GMT")))
+                .andDo(print())
     }
 }
