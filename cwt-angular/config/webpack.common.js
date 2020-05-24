4b5c0469af05f45cd56a88158cab4dc187847865
Zemke <florian@zemke.io>
2020-05-24 21:12:58 +0200

    More chunks

--- cwt-angular/config/webpack.common.js
+++ cwt-angular/config/webpack.common.js
@@ -29,7 +29,14 @@ module.exports = {
             cacheGroups: {
                 commons: {
                     test: /[\\/]node_modules[\\/]/,
-                    name: "vendor",
+                    name(module) {
+                        // get the name. E.g. node_modules/packageName/not/this/part.js
+                        // or node_modules/packageName
+                        const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+                        // npm package names are URL-safe, but some servers don't like @ symbols
+                        return `npm.${packageName.replace('@', '')}`;
+                    },
                     chunks: "initial",
                 },
             },
