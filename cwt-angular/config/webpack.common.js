8c26e9d82aa63f0945d5a9e81733a086026fd196
Zemke <florian@zemke.io>
2020-05-25 11:27:11 +0200

    Revert "More chunks"
    This reverts commit 4b5c0469af05f45cd56a88158cab4dc187847865.

--- cwt-angular/config/webpack.common.js
+++ cwt-angular/config/webpack.common.js
@@ -29,14 +29,7 @@ module.exports = {
             cacheGroups: {
                 commons: {
                     test: /[\\/]node_modules[\\/]/,
-                    name(module) {
-                        // get the name. E.g. node_modules/packageName/not/this/part.js
-                        // or node_modules/packageName
-                        const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-
-                        // npm package names are URL-safe, but some servers don't like @ symbols
-                        return `npm.${packageName.replace('@', '')}`;
-                    },
+                    name: "vendor",
                     chunks: "initial",
                 },
             },
