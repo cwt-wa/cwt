a201bb6358381cd6e9cf1119f0d5fa70772641a8
Zemke <florian@zemke.io>
2020-09-24 10:50:35 +0200

    Change theme light color

--- cwt-angular/src/main/webapp/tetris/color-scheme.ts
+++ cwt-angular/src/main/webapp/tetris/color-scheme.ts
@@ -1,6 +1,6 @@
 export const grid_background = "#1B2021";
 export const grid_stroke = "#000";
-export const lvl = "#C4C3C6";
+export const lvl = "#bb829b";
 
 export const square_figure = "#2A9D8F";
 export const l_figure = "#5E4A5A";
@@ -9,4 +9,4 @@ export const i_figure = "#9D2A38";
 export const t_figure = "#E9C46A";
 export const j_figure = "#655F59";
 export const z_figure = "#2A729D";
-export const little_square_figure = "#C4C3C6";
+export const little_square_figure = "#bb829b";
