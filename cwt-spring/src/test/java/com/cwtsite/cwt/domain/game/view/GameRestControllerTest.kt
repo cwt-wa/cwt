bbabb7ca4a6f88a2fe25f08d80e6d37b8e3f3692
Zemke <florian@zemke.io>
2020-11-10 01:51:52 +0100

    Some tests

--- cwt-spring/src/test/java/com/cwtsite/cwt/domain/game/view/GameRestControllerTest.kt
+++ cwt-spring/src/test/java/com/cwtsite/cwt/domain/game/view/GameRestControllerTest.kt
@@ -4,9 +4,12 @@ import com.cwtsite.cwt.controller.RestException
 import com.cwtsite.cwt.core.ClockInstance
 import com.cwtsite.cwt.core.event.SseEmitterFactory
 import com.cwtsite.cwt.core.event.stats.GameStatsEventListener
+import com.cwtsite.cwt.domain.bet.entity.Bet
 import com.cwtsite.cwt.domain.core.view.model.PageDto
 import com.cwtsite.cwt.domain.game.entity.Game
+import com.cwtsite.cwt.domain.game.entity.PlayoffGame
 import com.cwtsite.cwt.domain.game.service.GameService
+import com.cwtsite.cwt.domain.game.view.model.BetCreationDto
 import com.cwtsite.cwt.domain.game.view.model.GameDetailDto
 import com.cwtsite.cwt.domain.message.service.MessageService
 import com.cwtsite.cwt.domain.playoffs.service.TreeService
@@ -17,19 +20,19 @@ import com.cwtsite.cwt.test.EntityDefaults
 import com.cwtsite.cwt.test.MockitoUtils.anyObject
 import com.cwtsite.cwt.test.MockitoUtils.safeEq
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.assertThrows
 import org.junit.runner.RunWith
-import org.mockito.ArgumentMatchers.*
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito.`when`
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.*
 import org.mockito.junit.MockitoJUnitRunner
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.http.HttpStatus
 import java.util.*
+import javax.servlet.http.HttpServletRequest
 import kotlin.test.Test
 
 @RunWith(MockitoJUnitRunner::class)
@@ -103,5 +106,54 @@ class GameRestControllerTest {
                     listOf(EntityDefaults.user().id!!, EntityDefaults.user().id!!, EntityDefaults.user().id!!))
         }
         assertThat(exception2.status).isEqualTo(HttpStatus.BAD_REQUEST)
+    }
+
+    @Test
+    fun placeBetOnGame_works() {
+        val game = EntityDefaults.game(scoreHome = null, scoreAway = null, playoff = PlayoffGame(1, 1, 1))
+        val user = EntityDefaults.user()
+        val request = mock(HttpServletRequest::class.java)
+        val header = "Auth Bearer"
+        val dto = BetCreationDto(user.id!!, game.id!!, false)
+        val bet = Bet(id = 1, user = user, game = game, betOnHome = dto.betOnHome)
+        `when`(gameService.findById(anyLong())).thenReturn(Optional.of(game))
+        `when`(userService.getById(anyLong())).thenReturn(Optional.of(user))
+        `when`(request.getHeader(authService.tokenHeaderName)).thenReturn(header)
+        `when`(authService.getUserFromToken(header)).thenReturn(user)
+        `when`(gameService.placeBet(anyObject(), anyObject(), anyBoolean())).thenReturn(bet)
+        cut.placeBetOnGame(game.id!!, dto, request)
+        verify(gameService).placeBet(game, user, dto.betOnHome)
+    }
+
+    @Test
+    fun placeBetOnGame_alreadyPlayed() {
+        val game = EntityDefaults.game(playoff = PlayoffGame(1, 1, 1))
+        val user = EntityDefaults.user()
+        val request = mock(HttpServletRequest::class.java)
+        val header = "Auth Bearer"
+        val dto = BetCreationDto(user.id!!, game.id!!, false)
+        `when`(gameService.findById(anyLong())).thenReturn(Optional.of(game))
+        `when`(userService.getById(anyLong())).thenReturn(Optional.of(user))
+        `when`(request.getHeader(authService.tokenHeaderName)).thenReturn(header)
+        `when`(authService.getUserFromToken(header)).thenReturn(user)
+        assertThatThrownBy { cut.placeBetOnGame(game.id!!, dto, request) }
+                .isExactlyInstanceOf(RestException::class.java)
+                .satisfies { assertThat((it as RestException).status).isEqualTo(HttpStatus.BAD_REQUEST) }
+    }
+
+    @Test
+    fun placeBetOnGame_impersonate() {
+        val game = EntityDefaults.game(playoff = PlayoffGame(1, 1, 1))
+        val user = EntityDefaults.user()
+        val request = mock(HttpServletRequest::class.java)
+        val header = "Auth Bearer"
+        val dto = BetCreationDto(2, game.id!!, false)
+        `when`(gameService.findById(anyLong())).thenReturn(Optional.of(game))
+        `when`(userService.getById(anyLong())).thenReturn(Optional.of(user))
+        `when`(request.getHeader(authService.tokenHeaderName)).thenReturn(header)
+        `when`(authService.getUserFromToken(header)).thenReturn(user)
+        assertThatThrownBy { cut.placeBetOnGame(game.id!!, dto, request) }
+                .isExactlyInstanceOf(RestException::class.java)
+                .satisfies { assertThat((it as RestException).status).isEqualTo(HttpStatus.FORBIDDEN) }
     }
 }
