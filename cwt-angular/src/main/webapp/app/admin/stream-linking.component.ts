af42c836fdfc683d24bb75a718b1116b7d4606ef
Zemke <florian@zemke.io>
2020-11-12 21:35:31 +0100

    Also list linked streams

--- cwt-angular/src/main/webapp/app/admin/stream-linking.component.ts
+++ cwt-angular/src/main/webapp/app/admin/stream-linking.component.ts
@@ -27,10 +27,37 @@ import {finalize} from "rxjs/operators";
                 <form [formGroup]="forms[stream.id]" (ngSubmit)="submit(stream)" class="form-row">
                     <div class="col">
                         <input type="text" placeholder="Game ID" required
-                               [formControlName]="'gameId'" class="form-control"/>
+                               formControlName="gameId" class="form-control"/>
                     </div>
                     <div class="col">
                         <input type="submit" value="Submit" class="btn btn-primary"/>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <div class="row mt-4">
+            <div class="col">
+                <hr>
+            </div>
+        </div>
+
+        <h2 class="mt-4">Linked streams</h2>
+        <p class="lead">Streams that have already been linked.</p>
+
+        <div *ngFor="let stream of linkedStreams" class="row mt-5">
+            <div class="col-12">
+                <h5>{{stream.title}}</h5>
+                <p>from {{stream.createdAt | cwtDate}}</p>
+            </div>
+            <div class="col-12 col-sm-6">
+                <form [formGroup]="forms[stream.id]" (ngSubmit)="submit(stream)" class="form-row">
+                    <div class="col">
+                        <input type="text" placeholder="Game ID" required
+                               formControlName="gameId" class="form-control"/>
+                    </div>
+                    <div class="col">
+                        <input type="submit" value="Update" class="btn btn-primary"/>
                     </div>
                 </form>
             </div>
@@ -41,6 +68,7 @@ import {finalize} from "rxjs/operators";
 export class StreamLinkingComponent implements OnInit {
 
     streams: StreamDto[];
+    linkedStreams: StreamDto[];
     games: GameMinimalDto[];
     forms: { [p: string]: FormGroup } = {};
     linkingLoading = false;
@@ -57,7 +85,11 @@ export class StreamLinkingComponent implements OnInit {
                     .filter(s => s.game == null)
                     .sort((s1, s2) => (new Date(s2.createdAt).getTime()
                         - new Date(s1.createdAt).getTime()))
-                this.buildForm();
+                this.linkedStreams = res
+                    .filter(s => s.game != null)
+                    .sort((s1, s2) => (new Date(s2.createdAt).getTime()
+                        - new Date(s1.createdAt).getTime()))
+                this.buildForms();
             })
     }
 
@@ -69,10 +101,12 @@ export class StreamLinkingComponent implements OnInit {
         const gameId = this.forms[stream.id].get('gameId').value;
         const confirm = window.confirm(`Link game ${gameId} to stream\n“${stream.title}”?`);
         if (!confirm) return;
-        this.requestService.post(`stream/${stream.id}/game/${gameId}/link`)
-            .subscribe(() => {
-                this.toastr.success("Successfully linked game.");
-                this.streams.splice(this.streams.findIndex(s => s.id === stream.id), 1);
+            this.requestService.post(`stream/${stream.id}/game/${gameId}/link`)
+                .subscribe(() => {
+                    this.toastr.success("Successfully linked game.");
+                    const linked = this.streams.splice(
+                        this.streams.findIndex(s => s.id === stream.id), 1);
+                    this.linkedStreams.push(...linked);
             });
     }
 
@@ -88,12 +122,13 @@ export class StreamLinkingComponent implements OnInit {
             });
     }
 
-    private buildForm(): void {
+    private buildForms(): void {
         for (const stream of this.streams) {
-            this.forms[stream.id] = this.fb.group({
-                gameId: [''],
-                submit: [''],
-            })
+            this.forms[stream.id] = this.fb.group({gameId: ['']});
+        }
+        for (const stream of this.linkedStreams) {
+            this.forms[stream.id] = this.fb.group({gameId: ['']});
+            this.forms[stream.id].setValue({gameId: stream.game.id.toString()});
         }
     }
 }
