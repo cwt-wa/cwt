c9092b5e09e70f5df7a132519a727f75e763d27b
Zemke <florian@zemke.io>
2020-11-22 15:53:23 +0100

    What is what and whitespace

--- cwt-spring/src/main/java/com/cwtsite/cwt/domain/schedule/service/ScheduleService.kt
+++ cwt-spring/src/main/java/com/cwtsite/cwt/domain/schedule/service/ScheduleService.kt
@@ -1,5 +1,6 @@
 package com.cwtsite.cwt.domain.schedule.service
 
+import com.cwtsite.cwt.core.news.PublishNews
 import com.cwtsite.cwt.domain.schedule.entity.Schedule
 import com.cwtsite.cwt.domain.user.repository.entity.User
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,24 +12,28 @@ import java.util.*
 @Service
 class ScheduleService {
 
-    @Autowired private lateinit var scheduleRepository: ScheduleRepository
+    @Autowired
+    private lateinit var scheduleRepository: ScheduleRepository
 
-    fun save(author: User, opponent: User, appointment: Timestamp): Schedule = scheduleRepository.save(Schedule(
-            author = author,
-            homeUser = author,
-            awayUser = opponent,
-            appointment = appointment
-    ))
+    // called for when the schedule is created
+    fun save(author: User, opponent: User, appointment: Timestamp): Schedule =
+            scheduleRepository.save(Schedule(
+                    author = author,
+                    homeUser = author,
+                    awayUser = opponent,
+                    appointment = appointment))
 
-    fun delete(schedule: Schedule) {
-        scheduleRepository.delete(schedule)
-    }
+    // called when the schedule is deleted by the user or the game has been reported
+    fun delete(schedule: Schedule) =
+            scheduleRepository.delete(schedule)
 
-    fun findByPairing(user1: User, user2: User): Schedule? = scheduleRepository.findByPairing(user1, user2)
+    fun findByPairing(user1: User, user2: User): Schedule? =
+            scheduleRepository.findByPairing(user1, user2)
 
     fun findAll(): MutableList<Schedule> = scheduleRepository.findAll()
 
     fun findById(id: Long): Optional<Schedule> = scheduleRepository.findById(id)
 
+    // called when a stream is scheduled for the schedule or that scheduled stream is cancelled
     fun save(schedule: Schedule): Schedule = scheduleRepository.save(schedule)
 }
