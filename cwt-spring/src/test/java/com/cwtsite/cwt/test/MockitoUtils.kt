4c4f5fe5bb72fc2b3e3ad11237da71e004a1f8cc
Zemke <florian@zemke.io>
2020-11-24 01:57:20 +0100

    Created safeArgThat along the way

--- cwt-spring/src/test/java/com/cwtsite/cwt/test/MockitoUtils.kt
+++ cwt-spring/src/test/java/com/cwtsite/cwt/test/MockitoUtils.kt
@@ -1,6 +1,8 @@
 package com.cwtsite.cwt.test
 
 import org.mockito.ArgumentCaptor
+import org.mockito.ArgumentMatcher
+import org.mockito.ArgumentMatchers
 import org.mockito.ArgumentMatchers.eq
 import org.mockito.Mockito
 
@@ -15,6 +17,12 @@ object MockitoUtils {
     fun <T : Any> safeEq(value: T): T = eq(value) ?: value
 
     fun <T> capture(argumentCaptor: ArgumentCaptor<T>): T = argumentCaptor.capture()
+
+
+    fun <T> safeArgThat(matcher: ArgumentMatcher<T>): T {
+        ArgumentMatchers.argThat(matcher)
+        return uninitialized()
+    }
 
     @Suppress("UNCHECKED_CAST")
     private fun <T> uninitialized(): T = null as T
