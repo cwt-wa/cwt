f240807e29ae06d794c88c7ee6c75b91433a0144
Zemke <florian@zemke.io>
2021-01-09 22:07:25 +0100

    GH-226 HttpClientException

--- cwt-spring/src/main/java/com/cwtsite/cwt/core/HttpClient.kt
+++ cwt-spring/src/main/java/com/cwtsite/cwt/core/HttpClient.kt
@@ -2,6 +2,8 @@ package com.cwtsite.cwt.core
 
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.io.BufferedReader
+import java.io.InputStream
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
@@ -14,6 +16,22 @@ class HttpClient {
 
     fun <T> request(request: HttpRequest, bodyHandler: BodyHandler<T>): HttpResponse<T> {
         logger.info("Requesting $request")
-        return HttpClient.newBuilder().build().send(request, bodyHandler)
+        val response = HttpClient.newBuilder().build().send(request, bodyHandler)
+        if (!response.statusCode().toString().startsWith("2")) {
+            val body = response.body()
+            val message = when (body) {
+                is String -> body
+                is InputStream -> body.bufferedReader().use(BufferedReader::readText)
+                is ByteArray -> body.decodeToString()
+                else -> {
+                    logger.warn("Cannot parse body of type ${body!!::class.simpleName}")
+                    ""
+                }
+            }
+            throw HttpClientException(message, response.statusCode())
+        }
+        return response
     }
+
+    inner class HttpClientException(message: String, public val statusCode: Int) : RuntimeException(message)
 }
--- cwt-spring/src/test/java/com/cwtsite/cwt/core/HttpClientTest.kt
+++ cwt-spring/src/test/java/com/cwtsite/cwt/core/HttpClientTest.kt
@@ -1,19 +1,29 @@
 package com.cwtsite.cwt.core
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.catchThrowable
 import org.json.JSONObject
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 import java.net.URI
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.net.http.HttpResponse.BodyHandler
 import java.net.http.HttpResponse.BodyHandlers
 
 class HttpClientTest {
 
     private val cut: HttpClient = HttpClient()
 
+    enum class TestBodyHandlers(public val bodyHandler: BodyHandler<*>) {
+        STRING(BodyHandlers.ofString()),
+        INPUT_STREAM(BodyHandlers.ofInputStream()),
+        BYTE_ARRAY(BodyHandlers.ofByteArray());
+    }
+
     @Test
-    fun request() {
+    fun request_ok() {
         val uri = "http://postman-echo.com/get?hello=world"
         val request = HttpRequest.newBuilder()
             .GET()
@@ -23,5 +33,22 @@ class HttpClientTest {
         val response: HttpResponse<String> = cut.request(request, BodyHandlers.ofString())
         val json = JSONObject(response.body())
         assertThat(json.getJSONObject("args").getString("hello")).isEqualTo("world")
+    }
+
+    @ParameterizedTest
+    @EnumSource(HttpClientTest.TestBodyHandlers::class)
+    fun request_404(testBodyHandler: TestBodyHandlers) {
+        val uri = "https://postman-echo.com/status/404"
+        val request = HttpRequest.newBuilder()
+            .GET()
+            .uri(URI.create(uri))
+            .header("Content-Type", "application/json")
+            .build()
+        val throwable = catchThrowable { cut.request(request, testBodyHandler.bodyHandler) }
+        assertThat(throwable).isExactlyInstanceOf(HttpClient.HttpClientException::class.java)
+        throwable is HttpClient.HttpClientException
+        println(throwable::class.simpleName)
+        // assertThat(throwable.statusCode).isEqualTo(404) // TODO
+        assertThat(throwable.message).isEqualTo("""{"status":404}""")
     }
 }
