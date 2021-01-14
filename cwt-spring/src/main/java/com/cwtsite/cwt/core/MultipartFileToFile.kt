e6b56bf3e19dd4b224ce88936c1e71c0b5aa5685
Zemke <florian@zemke.io>
2021-01-15 00:02:55 +0100

    GH-266 Adressing compiler warnings
    Once deprecated function remains as its replacement is still
    experimental in Kotlin.

--- cwt-spring/src/main/java/com/cwtsite/cwt/core/MultipartFileToFile.kt
+++ cwt-spring/src/main/java/com/cwtsite/cwt/core/MultipartFileToFile.kt
@@ -2,13 +2,13 @@ package com.cwtsite.cwt.core
 
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
-import java.io.File.createTempFile
 
 object MultipartFileToFile {
 
      fun convertMultipartFileToFile(multipartFile: MultipartFile): File {
-        val tempFile = createTempFile()
+        val tempFile = File.createTempFile("cwt_", "_multipart")
         multipartFile.transferTo(tempFile)
         return tempFile
     }
 }
+
