b22837054584cd7b704ddeffa9a8a0181d0ec055
Zemke <florian@zemke.io>
2022-09-19 13:17:18 +0200

    Lookbehind now supported by Firefox
    https://www.mozilla.org/en-US/firefox/78.0/releasenotes/

--- cwt-angular/src/main/webapp/app/_util/convert-links.pipe.ts
+++ cwt-angular/src/main/webapp/app/_util/convert-links.pipe.ts
@@ -13,20 +13,12 @@ export class ConvertLinksPipe implements PipeTransform {
 
         const htmlClasses = classes.length ? ` class="${classes.join(" ")}" ` : " ";
 
-        try {
-            // Lookarounds are apparently only supported by Chrome.
-            let regexWithProtocol = new RegExp("(?<!@)(\\b(?:http[s]?:\\/\\/)?(?:www\\.)?[a-z0-9-]{3,}?\\.(?:[a-z]{2,})(?:[.a-z0-9-\\/]+)?(?:[^\\s]*)?\\b)", "gi");
-            value = value.replace(regexWithProtocol, `<a href="http://$1"${htmlClasses}target="_blank">$1</a>`);
-            value = value.replace(/http?:\/\/(http[s]?:\/\/)/gi, '$1');
+        const regexWithProtocol = new RegExp("(?<!@)(\\b(?:http[s]?:\\/\\/)?(?:www\\.)?[a-z0-9-]{3,}?\\.(?:[a-z]{2,})(?:[.a-z0-9-\\/]+)?(?:[^\\s]*)?\\b)", "gi");
+        value = value.replace(regexWithProtocol, `<a href="http://$1"${htmlClasses}target="_blank">$1</a>`);
+        value = value.replace(/http?:\/\/(http[s]?:\/\/)/gi, '$1');
 
-            const regexForMail = /(\b\S+@\S+\b)/g;
-            value = value.replace(regexForMail, '<a href="mailto:$1">$1</a>');
-        } catch (e) {
-            let regexWithProtocol = /(\b(?:http[s]?:\/\/)?(?:www\.)?[a-z0-9-]{3,}?\.(?:[a-z]{2,})(?:[.a-z0-9-\/]+)?(?:[^\s]*)?\b)/gi;
-            value = value.replace(regexWithProtocol, `<a href="http://$1"${htmlClasses}target="_blank">$1</a>`);
-            value = value.replace(/http?:\/\/(http[s]?:\/\/)/gi, '$1');
-            return value;
-        }
+        const regexForMail = /(\b\S+@\S+\b)/g;
+        value = value.replace(regexForMail, '<a href="mailto:$1">$1</a>');
 
         return value;
     }
