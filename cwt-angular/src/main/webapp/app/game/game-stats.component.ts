a7d6f8e419dfe1f5d6b39870e307320967e7026b
Zemke <florian@zemke.io>
2022-10-18 12:48:46 +0200

    Average symbol (empty) only for Germany

--- cwt-angular/src/main/webapp/app/game/game-stats.component.ts
+++ cwt-angular/src/main/webapp/app/game/game-stats.component.ts
@@ -161,7 +161,7 @@ const colors: { [key: string]: string } = {
             </div>
             <div class="head" [ngStyle]="{'background-image': linearGradientHealthPoints(0)}">
                 <div class="user" *ngFor="let averageTurnTime of averageTurnTimes;">
-                    &empty; {{averageTurnTime || 0}}s
+                    <small>avg.</small> {{averageTurnTime || 0}}s
                 </div>
             </div>
             <div class="head">
