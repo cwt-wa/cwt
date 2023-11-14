9cb144ed57d84e4c10c3384961d209a70d81676e
Zemke <florian@zemke.io>
2023-11-14 15:02:14 +0100

    serialize json array

--- cwt-spring/src/main/java/com/cwtsite/cwt/domain/notification/service/PushNotitification.kt
+++ cwt-spring/src/main/java/com/cwtsite/cwt/domain/notification/service/PushNotitification.kt
@@ -1,12 +1,15 @@
 package com.cwtsite.cwt.domain.notification.service
 
+import org.json.JSONArray
+import org.json.JSONObject
+
 data class PushNotification(
     val title: String,
     val body: String? = null,
     val tag: String? = null,
 ) {
     fun toRequest(subscriptions: List<String>): Map<String, Any> = mapOf(
-        "subs" to subscriptions,
+        "subs" to JSONArray(subscriptions.map { JSONObject(it) }),
         "notification" to mapOf(
             "title" to title,
             "options" to mapOf(
